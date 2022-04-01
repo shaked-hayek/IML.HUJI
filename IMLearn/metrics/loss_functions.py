1e2759a4cbcffe9d0214443755edcf2f8390beb0
unknown <shaked.hayek@mail.huji.ac.il>
2022-04-01 22:31:03 +0300

    Added MSE

--- IMLearn/metrics/loss_functions.py
+++ IMLearn/metrics/loss_functions.py
@@ -16,7 +16,8 @@ def mean_square_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     -------
     MSE of given predictions
     """
-    raise NotImplementedError()
+    arrays_sub = np.subtract(y_true, y_pred)
+    return sum(np.multiply(arrays_sub, arrays_sub)) / len(y_true)
 
 
 def misclassification_error(y_true: np.ndarray, y_pred: np.ndarray, normalize: bool = True) -> float:
