c617236b4ec0548d08215c5e524d13a245c5fb41
unknown <shaked.hayek@mail.huji.ac.il>
2022-04-04 13:51:38 +0300

    Q4

--- exercises/city_temperature_prediction.py
+++ exercises/city_temperature_prediction.py
@@ -50,15 +50,29 @@ if __name__ == '__main__':
     df[YEAR] = df[YEAR].astype(str)
     df_israel = df.loc[df[COUNTRY] == ISRAEL]
 
-    px.scatter(df_israel, x=DAY_OF_YEAR, y=TEMP, color=YEAR,
-               title="Average daily temperature change as a function of the day in year").show()
-
-    df_month_grouped = df_israel.groupby(MONTH).agg(np.std)
-    fig = px.bar(df_month_grouped, y=TEMP, labels={"Temp": "STD of temp"},
-                 title="Each month the standard deviation of the daily temperatures").show()
-
-    # Question 3 - Exploring differences between countries
+    # px.scatter(df_israel, x=DAY_OF_YEAR, y=TEMP, color=YEAR,
+    #            title="Average daily temperature change as a function of the day in year").show()
+    #
+    # df_month_grouped = df_israel.groupby(MONTH).agg(np.std)
+    # px.bar(df_month_grouped, y=TEMP, labels={"Temp": "STD of temp"},
+    #        title="Each month the standard deviation of the daily temperatures").show()
+    #
+    # # Question 3 - Exploring differences between countries
+    # df_grouped_3 = df.groupby([MONTH, COUNTRY]).Temp.agg([np.mean, np.std])
+    # px.line(df_grouped_3, x=df_grouped_3.axes[0].get_level_values(0), y="mean", error_y="std",
+    #         color=df_grouped_3.axes[0].get_level_values(1), labels={"x": "Month", "mean": "Temp mean"},
+    #         title="Average monthly temperature (with STD and country coded)").show()
 
     # Question 4 - Fitting model for different values of `k`
+    train_X, train_y, test_X, test_y = split_train_test(df_israel[DAY_OF_YEAR], df_israel[TEMP])
+    loss_l = []
+    for k in range(1, 11):
+        model = PolynomialFitting(k)
+        model.fit(train_X.to_numpy(), train_y.to_numpy())
+        loss = model.loss(test_X.to_numpy(), test_y.to_numpy())
+        print("For k={0} loss is {1:.2f}".format(k, loss))
+        loss_l.append(loss)
+    px.bar(x=list(range(1, 11)), y=loss_l, labels={"x": "k (degree)", "y": "Loss"},
+           title="Loss for each value of k (degree)").show()
 
-    # Question 5 - Evaluating fitted model on different countries
+# Question 5 - Evaluating fitted model on different countries
