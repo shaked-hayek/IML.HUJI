9c2540611dce0130c167a787a61239af549b950d
unknown <shaked.hayek@mail.huji.ac.il>
2022-06-05 16:01:39 +0300

    Finised part 1

--- IMLearn/model_selection/cross_validate.py
+++ IMLearn/model_selection/cross_validate.py
@@ -5,6 +5,22 @@ import numpy as np
 from IMLearn import BaseEstimator
 
 
+def split_xy(s):
+    return s[:, :-1], s[:, -1]
+
+
+def split_k_folds(X: np.ndarray, y: np.ndarray, k: int):
+    S = np.c_[X, y]
+    return np.array_split(S, k, axis=0)
+
+
+def get_train_test(folds, i):
+    c_folds = deepcopy(folds)
+    test_X, test_y = split_xy(c_folds.pop(i))
+    train_X, train_y = split_xy(np.concatenate(c_folds))
+    return train_X.reshape((train_X.shape[0],)), train_y, test_X.reshape((test_X.shape[0],)), test_y
+
+
 def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
                    scoring: Callable[[np.ndarray, np.ndarray, ...], float], cv: int = 5) -> Tuple[float, float]:
     """
@@ -37,4 +53,15 @@ def cross_validate(estimator: BaseEstimator, X: np.ndarray, y: np.ndarray,
     validation_score: float
         Average validation score over folds
     """
-    raise NotImplementedError()
+    folds = split_k_folds(X, y, cv)
+    train_scores = []
+    validation_scores = []
+    for i in range(cv):
+        train_X, train_y, test_X, test_y = get_train_test(folds, i)
+        estimator.fit(train_X, train_y)
+        pred_y = estimator.predict(test_X)
+        train_pred_y = estimator.predict(train_X)
+        train_scores.append(scoring(train_y, train_pred_y))
+        validation_scores.append(scoring(test_y, pred_y))
+    return np.average(train_scores), np.average(validation_scores)
+
--- exercises/perform_model_selection.py
+++ exercises/perform_model_selection.py
@@ -13,6 +13,27 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 
+TRAIN_PROPORTION = 2 / 3
+K_MIN = 0
+K_MAM = 11
+
+
+def split_and_get_array(X, y, train_proportion):
+    train_X, train_y, test_X, test_y = split_train_test(pd.DataFrame(X), pd.DataFrame(y),
+                                                        train_proportion=train_proportion)
+    n_samples_train = train_X.shape[0]
+    n_samples_test = test_X.shape[0]
+    train_X = train_X.to_numpy().reshape((n_samples_train,))
+    train_y = train_y.to_numpy().reshape((n_samples_train,))
+    test_X = test_X.to_numpy().reshape((n_samples_test,))
+    test_y = test_y.to_numpy().reshape((n_samples_test,))
+    return train_X, train_y, test_X, test_y
+
+
+def x_func(x):
+    return (x + 3) * (x + 2) * (x + 1) * (x - 1) * (x - 2)
+
+
 def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
     """
     Simulate data from a polynomial model and use cross-validation to select the best fitting degree
@@ -27,13 +48,50 @@ def select_polynomial_degree(n_samples: int = 100, noise: float = 5):
     """
     # Question 1 - Generate dataset for model f(x)=(x+3)(x+2)(x+1)(x-1)(x-2) + eps for eps Gaussian noise
     # and split into training- and testing portions
-    raise NotImplementedError()
+    X = np.linspace(-1.2, 2, n_samples)
+    y = x_func(X)
+    y_noise = y + np.random.normal(0, scale=noise, size=len(y))
+    train_X, train_y, test_X, test_y = split_and_get_array(X, y_noise, train_proportion=TRAIN_PROPORTION)
+
+    # go.Figure([
+    #         go.Scatter(x=X, y=y, mode="markers+lines", name="Real Points",
+    #                    marker=dict(color="black", opacity=.7)),
+    #         go.Scatter(x=train_X, y=train_y, mode="markers+lines", name="Train Points",
+    #                    marker=dict(color="red", opacity=.7)),
+    #         go.Scatter(x=test_X, y=test_y, mode="markers+lines", name="Test Points",
+    #                    marker=dict(color="blue", opacity=.7))
+    # ], layout=go.Layout(
+    #         title={"text": "True, Train and Test Data"},
+    #         xaxis={"title": r"$x$"},
+    #         yaxis={"title": r"$y$"})).show()
 
     # Question 2 - Perform CV for polynomial fitting with degrees 0,1,...,10
-    raise NotImplementedError()
+    avg_scores = []
+    for k in range(K_MIN, K_MAM):
+        poly_fit = PolynomialFitting(k)
+        avg_scores.append(cross_validate(poly_fit, train_X, train_y, mean_square_error, cv=5))
+    avg_scores_nd = np.array(avg_scores)
+
+    go.Figure([
+        go.Scatter(x=list(range(K_MIN, K_MAM)), y=avg_scores_nd[:, 0], mode="markers+lines", name="Average Train Scores",
+                   marker=dict(color="black", opacity=.7)),
+        go.Scatter(x=list(range(K_MIN, K_MAM)), y=avg_scores_nd[:, 1], mode="markers+lines", name="Average Validation Scores",
+                   marker=dict(color="red", opacity=.7)),
+    ], layout=go.Layout(
+        title={"text": "Average Train and Validation Scores"},
+        xaxis={"title": "k"},
+        yaxis={"title": r"score"})).show()
 
     # Question 3 - Using best value of k, fit a k-degree polynomial model and report test error
-    raise NotImplementedError()
+    avg_validation_scores = avg_scores_nd[:, 1]
+    best_k = int(np.argmin(avg_validation_scores))
+    print("The k that minimizes the validation score: {0}".format(best_k))
+
+    best_poly_fit = PolynomialFitting(best_k)
+    best_poly_fit.fit(train_X, train_y)
+    pred_y = best_poly_fit.predict(test_X)
+    print("The test error of the fitted model: {0}".format(mean_square_error(test_y, pred_y)))
+    print("The validation error previously achieved with this k: {0}".format(avg_validation_scores[best_k]))
 
 
 def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 500):
@@ -61,4 +119,5 @@ def select_regularization_parameter(n_samples: int = 50, n_evaluations: int = 50
 
 if __name__ == '__main__':
     np.random.seed(0)
-    raise NotImplementedError()
+    select_polynomial_degree()
+    # select_regularization_parameter()
